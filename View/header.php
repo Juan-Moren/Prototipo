b46f19cefeef082c1e9d84fc7a243959bde922f6
Juan-Moren <juan.morenobaquero@unicolombo.edu.co>
2025-09-26 11:55:08 -0500

    complementacion

---
+++ View/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>SYSNET IPS</title>
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+<div class="container">
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . "/Controller/CitaController.php";
+
+$controller = new CitaController();
+$action = $_GET['action'] ?? '';
+
+if ($action == 'nueva') {
+    include "View/cita_form.php";
+} elseif ($action == 'crear') {
+    $paciente = $_POST['paciente'];
+    $especialidad = $_POST['especialidad'];
+    $fecha = $_POST['fecha'];
+    $controller->crearCita($paciente, $especialidad, $fecha);
+    $citas = $controller->listarCitas();
+    include "View/cita_lista.php";
+} else {
+    $citas = $controller->listarCitas();
+    include "View/cita_lista.php";
+}
